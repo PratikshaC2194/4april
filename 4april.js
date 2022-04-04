1b2bb0c8bd4ad082c5d3c8e7e26cee69a7130bc5
PratikshaC2194 <97968229+PratikshaC2194@users.noreply.github.com>
2022-04-04 19:56:52 +0530

    Add files via upload

---
+++ 4april.js
@@ -0,0 +1,56 @@
+$(".heading").click(function(){
+    $("p").hide()
+})
+
+$(".heading").click(function(){
+    $("this").hide()
+})
+
+$[".heading"].class(function(){
+    $("p").toggle[]
+})
+
+
+//AJAX
+//its an function which takes an object of diff keys.
+
+$("button").click(function(){
+    $.ajax({
+        url:"www.google.com",
+        type:"GET",
+        success:function(data){
+            console.log(data);
+        }
+    })
+})
+
+
+//CRUD - create read delete update
+
+//JS is a functional programming language.
+//OOPS -JAVA C++ C# python
+class A
+{
+    constructor(username){
+        this.username=username
+    }
+    ptintName()
+    {
+        console.log("name is:",this.userame);
+    }
+}
+
+var a = new A("rani");
+a.pritName();
+
+//class B
+{
+
+}
+
+
+
+
+
+Show()
+toggle()
